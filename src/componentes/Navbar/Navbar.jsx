55a3587f6e820ae53387f72ae3bd322803ca4e6d
nusito21 <josegregoriojg8766@gmail.com>
2023-05-03 12:02:30 -0400

    imagen

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -4,11 +4,12 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import './NavBar.css'
 
+
 const NavBar = () => {
 
   
 
-  const img = <img className='img-prof' src="../src/assets/perro.jpg" />
+  const img = <img className='img-prof' src="./assets/perro.jpg" />
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
       <div className="container-fluid" >
