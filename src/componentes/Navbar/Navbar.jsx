2de0cced339b456515f4bef4ac2a29bf04d6555b
nusito21 <josegregoriojg8766@gmail.com>
2023-05-04 15:18:04 -0400

    ico

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar
+            My Home
           </a>
           <button
             className="navbar-toggler collapsed"
