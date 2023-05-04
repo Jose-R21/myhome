e74c62599cc60c0222fd5d80ab34588c37537462
nusito21 <josegregoriojg8766@gmail.com>
2023-05-04 08:50:57 -0400

    mobile-navbar

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,53 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import perro from '../../assets/perro.jpg';
-import './NavBar.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import perro from "../../assets/perro.jpg";
+import "./NavBar.css";
 
 const NavBar = () => {
-
-  
-
-  const img = <img className='img-prof' src={perro} />
+  const img = <img className="img-prof" src={perro} />;
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
-      <div className="container-fluid" >
-        <Link className="navbar-brand" to="/">MERN</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarColor01">
-          <ul className="navbar-nav ml-auto me-auto ul-princ">
-            <div className="collapse navbar-collapse">
-            <li className="nav-item">
-                <Link className="nav-link active" to="/myhome">Inicio
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor02"
+            aria-controls="navbarColor02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="navbar-collapse collapse" id="navbarColor02">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/myhome">
+                  Inicio
                   <span className="visually-hidden">(current)</span>
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link active" to="/myhome/calculadora">Calculadora
+                <Link className="nav-link active" to="/myhome/calculadora">
+                  Calculadora
                   <span className="visually-hidden">(current)</span>
                 </Link>
               </li>
-            </div>
-            <div className="collapse ml-auto navbar-collapse justify-content-end">
-              
-              <li className="dropdown navbar-light bg-dark" >
-                <DropdownButton title={img}  >
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </DropdownButton>
-              </li>
-            </div>
-          </ul>
-
+            </ul>
+          </div>
+          <div className="collapse ml-auto navbar-collapse justify-content-end">
+            <li className="dropdown navbar-light bg-dark">
+              <DropdownButton title={img}>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </DropdownButton>
+            </li>
+          </div>
         </div>
-      </div>
-    </nav>
-  )
-}
+      </nav>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
