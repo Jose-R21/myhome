45762d82f323f5bf89414147499516d702f48060
nusito21 <josegregoriojg8766@gmail.com>
2023-05-12 09:18:41 -0400

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 
-//importScripts("https://www.gstatic.com/firebasejs/9.21.0/firebase-app-compat.js")
-//importScripts("https://www.gstatic.com/firebasejs/9.21.0/firebase-messaging-compat.js")
+importScripts('https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.21.0/firebase-messaging.js');
 
 
 
